fdd429c7ad340c713b5dc36122bc3123ffd2829e
Suprasanna Bhaumik <suprasanna.bhaumik86@gmail.com>
2019-01-30 11:04:46 +0530

    Initial commit

---
+++ src/main/java/com/library/user/LibraryManagementBooksCommandApplication.java
@@ -0,0 +1,14 @@
+package com.library.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementBooksCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementBooksCommandApplication.class, args);
+	}
+
+}
+
